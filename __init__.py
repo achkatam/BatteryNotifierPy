8eb3abfe88cc5bd5286d1c8d5d15e215e4b2ae33
achkatam <agmatevv@gmail.com>
2023-09-15 13:39:58 -0400

    added one more file. Working over creating hidden icon with exit func

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 import time
 from notification import notify
 from get_battery_info import get_battery_info
+import sys
 import create_icon
 
 if __name__ == "__main__":
@@ -9,9 +10,13 @@ if __name__ == "__main__":
     power_plugged = battery_info[0]
     battery_percent = battery_info[1]
 
-    while battery_percent < 80:
-        if power_plugged is True and battery_percent >= 77:
+    while True:
+        if power_plugged is False and battery_percent == 51:
             notify()
-            create_icon.create_system_icon()
-            time.sleep(60)
-            battery_info = get_battery_info()
+            time.sleep(5)
+            sys.exit()
+        else:
+            time.sleep(5)
+        battery_info = get_battery_info()
+        power_plugged = battery_info[0]
+        battery_percent = battery_info[1]
